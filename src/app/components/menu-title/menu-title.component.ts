10470652ae5554676aa89323c36c73d480c03d67
gilvan <gilvanlaurentino1979@gmail.com>
2023-11-25 13:34:26 -0300

    Continuacao Projeto...

---
+++ src/app/components/menu-title/menu-title.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-menu-title',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './menu-title.component.html',
+  styleUrl: './menu-title.component.css'
+})
+export class MenuTitleComponent {
+
+}
